716c82796326fa1d519693ef6e2de2fab650eeca
Fabrício M. Damasceno <fabricio.devwebrj@gmail.com>
2024-09-23 17:42:03 -0300

    Criando mapeamento através do DbContext

---
+++ CatalogWebAPI/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CatalogWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogWebAPI.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options ) : base( options )
+    {        
+    }
+
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product>? Products { get; set; }
+
+
+}
