d3387df099cc4d43e519b2485ac792b0d6bf3470
Fabrício M. Damasceno <fabricio.devwebrj@gmail.com>
2024-09-27 19:34:14 -0300

    Adicionando ProductsController - Get

---
+++ CatalogWebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using CatalogWebAPI.Context;
+using CatalogWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogWebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            var products = _context?.Products?.ToList();
+            if (products is null)
+                return NotFound();
+            return products;
+        }
+    }
+}
