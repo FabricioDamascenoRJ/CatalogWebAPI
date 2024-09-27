6a2ed6642295f185975cd339992f8d887d9402c5
Fabrício M. Damasceno <fabricio.devwebrj@gmail.com>
2024-09-27 07:30:53 -0300

    Criando Migrations para popular as tabelas Products e Categories

---
+++ CatalogWebAPI/Migrations/20240927101424_PopularProducts.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into products(Name, Description, Price, ImageUrl, Stock, CreatedAt, CategoryId) " +
+                "Values('Coca-cola Diet', 'Refrigerante lata de 350ml', 5.45, 'cocacola.jpg', 50, now(), 1)");
+
+            mb.Sql("Insert into products(Name, Description, Price, ImageUrl, Stock, CreatedAt, CategoryId) " +
+                 "Values('Lanche de Atum','Lanche de Atum com maionese',8.50,'atum.jpg',10,now(),2)");
+
+            mb.Sql("Insert into products(Name, Description, Price, ImageUrl, Stock, CreatedAt, CategoryId) " +
+                "Values('Pudim 100 g','Pudim de leite condensado 100g',6.75,'pudim.jpg',20,now(),3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from products");
+        }
+    }
+}
