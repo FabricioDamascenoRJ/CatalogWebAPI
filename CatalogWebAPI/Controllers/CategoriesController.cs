f96f05c684daeb7def54cd9d58391d7b3371470b
Fabrício M. Damasceno <fabricio.devwebrj@gmail.com>
2024-09-27 23:28:12 -0300

    Adicionando Try Catch no CategoriesController

--- CatalogWebAPI/Controllers/CategoriesController.cs
+++ CatalogWebAPI/Controllers/CategoriesController.cs
@@ -19,66 +19,119 @@ namespace CatalogWebAPI.Controllers
         [HttpGet("Products")]
         public ActionResult<IEnumerable<Category>> GetCategoriesProducts()
         {
-            return _context.Categories
+            try
+            {
+                return _context.Categories
                 .Include(p => p.Products)
                 .AsNoTracking()
                 .ToList();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                        "Ocorreu um erro interno ao tratar a sua solicitação. Favor contactar o Adminstrador do sistema.");
+            }
+            
         }
 
         [HttpGet]
         public ActionResult<IEnumerable<Category>> GetAll()
         {
-            return _context.Categories
+            try
+            {
+                return _context.Categories
                 .AsNoTracking()
                 .ToList();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                        "Ocorreu um erro interno ao tratar a sua solicitação. Favor contactar o Adminstrador do sistema.");
+            }            
         }
 
         [HttpGet("{id:int}", Name = "GetCategories")]
         public ActionResult<Category> GetById(int id)
         {
-            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
-            if (category == null)
-                return NotFound("Categoria não encontrada.");
-            return Ok(category);
+            try
+            {
+                var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+                if (category == null)
+                    return NotFound($"Categoria com o id={id} não encontrada.");
+                return Ok(category);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                        "Ocorreu um erro interno ao tratar a sua solicitação. Favor contactar o Adminstrador do sistema.");
+            }
+            
         }
 
         [HttpPost]
         public ActionResult Post(Category category)
         {
-            if (category is null)
-                return BadRequest();
+            try
+            {
+                if (category is null)
+                    return BadRequest("Dados inválidos");
 
-            _context.Categories.Add(category);
-            _context.SaveChanges();
+                _context.Categories.Add(category);
+                _context.SaveChanges();
 
-            return new CreatedAtRouteResult("GetCategory",
-                new { id = category.Id }, category);
+                return new CreatedAtRouteResult("GetCategory",
+                    new { id = category.Id }, category);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                        "Ocorreu um erro interno ao tratar a sua solicitação. Favor contactar o Adminstrador do sistema.");
+            }
+            
         }
 
         [HttpPut("{id:int}")]
         public ActionResult Put(int id, Category category)
         {
-            if (id != category.Id)
-                return BadRequest();
+            try
+            {
+                if (id != category.Id)
+                    return BadRequest("Dados inválidos");
 
-            _context.Entry(category).State = EntityState.Modified;
-            _context.SaveChanges();
+                _context.Entry(category).State = EntityState.Modified;
+                _context.SaveChanges();
 
-            return Ok(category);
+                return Ok(category);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                        "Ocorreu um erro interno ao tratar a sua solicitação. Favor contactar o Adminstrador do sistema.");
+            }
+            
         }
 
         [HttpDelete("{id:int}")]
         public ActionResult Delete(int id)
         {
-            var category = _context.Categories.FirstOrDefault(p => p.Id == id);
+            try
+            {
+                var category = _context.Categories.FirstOrDefault(c => c.Id == id);
 
-            if (category == null)
-                return NotFound("Categoria não encontrada.");
+                if (category == null)
+                    return NotFound($"Categoria com id={id} não encontrada.");
 
-            _context.Categories.Remove(category);
-            _context.SaveChanges();
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
 
-            return Ok(category);
+                return Ok(category);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                        "Ocorreu um erro interno ao tratar a sua solicitação. Favor contactar o Adminstrador do sistema.");
+            }
         }
     }
 }
