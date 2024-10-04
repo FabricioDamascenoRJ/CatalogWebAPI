d91378c033f6e4779e61f61bc38e08224308fa98
Fabrício M. Damasceno <fabricio.devwebrj@gmail.com>
2024-10-03 23:59:54 -0300

    Criando um log personalizado

---
+++ CatalogWebAPI/Logging/CustomerLogger.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+
+namespace CatalogWebAPI.Logging;
+
+public class CustomerLogger : ILogger
+{
+    readonly string loggerName;
+    readonly CustomLoggerProviderConfiguration loggerConfig;
+
+    public CustomerLogger(string name, CustomLoggerProviderConfiguration config)
+    {
+        loggerName = name;
+        loggerConfig = config;
+    }
+
+    public IDisposable BeginScope<TState>(TState state)
+    {
+        return null;
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return logLevel == loggerConfig.LogLevel;
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state,
+            Exception exception, Func<TState, Exception, string> formatter)
+    {
+        string mensage = $"{logLevel.ToString()}: {eventId.Id} - {formatter(state, exception)}";
+
+        WriteTexttoFile(mensage);
+    }
+
+    private void WriteTexttoFile(string mensage)
+    {
+        string pathFileLog = @"CatalogWebAPI_log.txt";
+        using (StreamWriter streamWriter = new StreamWriter(pathFileLog, true))
+        {
+            try
+            {
+                streamWriter.WriteLine(mensage);
+                streamWriter.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
